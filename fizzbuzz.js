cab4abb76f6c6f5d0ebc7e24146964546287db62
evcaho <evcaldwell@gmail.com>
2015-06-28 17:43:07 -0700

    Updated to enter FizzBuzz code

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,1 +1,10 @@
-// Place your FizzBuzz code here. 
+for (var i = 1; i <= 20; i++) { 
+if ((i % 3 == 0) && (i % 5 == 0)) {
+    console.log ('fizzbuzz')} 
+else if (i % 3 == 0) {
+    console.log ('fizz')
+   } else if (i % 5 == 0) {
+    console.log ('buzz')
+}  
+    else console.log (i)
+}
